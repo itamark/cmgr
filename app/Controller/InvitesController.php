1b8410a14cf50c541da3e25316a32e2818016751
Itamar <itamar@outlook.com>
2015-03-15 01:26:33 -0400

    adding invite stuff

---
+++ app/Controller/InvitesController.php
@@ -0,0 +1,74 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Items Controller
+ *
+ * @property Item $Item
+ * @property PaginatorComponent $Paginator
+ */
+class InvitesController extends AppController {
+
+    public function index() {
+        // $this->set(
+        //      'UpvoteList',
+        //      $this->Upvote->find('all')
+        //  );
+    }
+
+    public function invite(){
+    	if($this->request->is('post')){
+    		if($this->Invite->save($this->request->data)){
+    			$invite = $this->Invite->read(null, $this->Invite->id);
+    			$email = new CakeEmail();
+			$email->template('invite', 'default')
+					->config('default')
+					->emailFormat('html')
+					->subject(__('You are invited to join ' . Configure::read('Application.name')))
+					->to($invite['Invite']['invitee_email'])
+					// ->from(Configure::read('Application.from_email'))
+					->from(AuthComponent::user('email'))
+					->send();
+
+    		}
+    	}
+
+    }
+
+   
+
+//     public function vote() {
+//         if ($this->request->is('post')) {
+//             // $something = $this->Upvote->find('count', array('Upvote.item_id' => 33, 'Upvote.user_id' =>  6));
+//                         // $something = $this->Upvote->find('all', array('Upvote.item_id' => $this->request->data['item_id'], 'Upvote.user_id' =>  AuthComponent::user('id')));
+
+//             $myvotes = $this->Upvote->find('count', array(
+//        'conditions' => array('Upvote.item_id' => $this->request->data['item_id'], 'Upvote.user_id' => AuthComponent::user('id'))
+//     ));
+//             // die(json_encode($something));
+// $callback = array();
+//     if($myvotes === 0){
+//         if ($this->Upvote->save($this->request->data)) {
+//             $callback['action'] = 'upvoted';
+//             $callback['count'] = $this->Upvote->find('count', array(
+//        'conditions' => array('Upvote.item_id ' => $this->request->data['item_id'])
+//     ));
+//                 header('Content-type: application/json');
+//                 die(json_encode($callback));
+//             }
+//         } else {
+//             if($this->Upvote->undo($this->request->data['item_id'], AuthComponent::user('id'))){
+//                 $callback['action'] = 'undone';
+//                 $callback['count'] = $this->Upvote->find('count', array(
+//        'conditions' => array('Upvote.item_id ' => $this->request->data['item_id'])
+//     ));
+//                 header('Content-type: application/json');
+//                 die(json_encode($callback));
+//             }
+//         }
+
+//         }
+    
+            
+        
+//     }
+}
