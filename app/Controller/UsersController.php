63dfe3c6fcddfb58e05e702ec597c597761f3de5
Itamar <itamar@outlook.com>
2015-07-13 00:03:38 +0300

    Admin and last_login fixes

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,5 +1,7 @@
 <?php
+App::uses('Sanitize', 'Utility');
 class UsersController extends AppController {
+	public $components = array('Cookie');
 	public function beforeFilter() {
 		parent::beforeFilter();
 		$this->Auth->allow('add', 'logout', 'change_password', 'remember_password', 'remember_password_step_2', 'view', 'opauth_complete', 'thanks', 'invite');
@@ -22,7 +24,7 @@ class UsersController extends AppController {
 		);
 // if the user exists by email
 		if ($this->User->hasAny($conditions)) {
-
+			$this->Cookie->write('invited', 'true');
 			$user = $this->User->find('first', array('conditions' => array('User.username' => $inviter), 'fields' => array('id', 'image', 'first_name', 'last_name')));
 
 			$id = $user['User']['id'];
@@ -84,8 +86,20 @@ class UsersController extends AppController {
 				array('id' => $id)
 			);
 			unset($this->request->data['User']['password']);
+			if ($this->Cookie->read('invited')) {
+				$user['User']['has_access'] = true;
+				// $this->User->saveField('has_access', true);
+				$this->User->updateAll(
+					array('has_access' => true)
+				);
+			}
+
 			if ($user['User']['has_access']) {
 				$this->Auth->login($this->request->data['User']);
+				// $this->User->saveField('last_login', date(DATE_ATOM));
+				$this->User->id = $user['User']['id'];
+				$now = date('Y-m-d H:i:s');
+				$this->User->saveField('last_login', $now);
 				return $this->redirect('/');
 			} else {
 				$this->Auth->login($this->request->data['User']);
@@ -116,6 +130,9 @@ class UsersController extends AppController {
 					// if (isset($this->data['auth']['info']['image'])) {
 					// 	copy($this->data['auth']['info']['image'], '../webroot/img/users/' . $id . '.jpg');
 					// }
+					$this->User->id = $id;
+				$now = date('Y-m-d H:i:s');
+				$this->User->saveField('last_login', $now);
 
 					$this->request->data['User'] = array_merge(
 						$this->request->data['User'],
@@ -259,6 +276,34 @@ class UsersController extends AppController {
 		}
 		$this->set('label', 'Edit user');
 		$this->render('_form');
+	}
+
+	public function grant($id = null) {
+		if (AuthComponent::user('role') != 'admin') {
+			throw new ForbiddenException("You're not allowed to do this.");
+		}
+		$this->User->id = $id;
+
+		if ($this->User->saveField('has_access', true)) {
+			return $this->redirect('/usersAdmin/index');
+		} else {
+			return $this->redirect('/');
+		}
+
+	}
+
+	public function revoke($id = null) {
+		if (AuthComponent::user('role') != 'admin') {
+			throw new ForbiddenException("You're not allowed to do this.");
+		}
+		$this->User->id = $id;
+
+		if ($this->User->saveField('has_access', false)) {
+			return $this->redirect('/usersAdmin/index');
+		} else {
+			return $this->redirect('/');
+		}
+
 	}
 
 	public function delete($id = null) {
